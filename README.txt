fc7e00dce77631249c65534331aa4958c616bc9c
brownbagmarketing <brownbagmarketing@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-23 17:50:26 +0000

    1.5.0 adding wizard and bugfixes
    git-svn-id: https://plugins.svn.wordpress.org/greenhouse-job-board/trunk@1143534 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- README.txt
+++ README.txt
@@ -3,8 +3,8 @@ Contributors: brownbagmarketing
 Donate link: https://www.brownbagmarketing.com/
 Tags: greenhouse, job board, api, resume
 Requires at least: 3.0.1
-Tested up to: 4.1.1
-Stable tag: 1.4.0
+Tested up to: 4.1.2
+Stable tag: 1.5.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -17,12 +17,12 @@ Plugin to pull a job board from greenhouse.io via their API and display it on yo
 Requirements:
 
 *	Must have a greenhouse account.
-*	Know your URL Token
+*	Know your URL Token.
 
 Initial Setup:
 
-*	For ease of use, setup your URL Token within Settings->Greenhouse Job Board Settings. (NOTE that you can also set this inline within a shortcode)
-*	API Key can also be set, but is not yet supported.
+*	For ease of use, setup your URL Token within Settings->Greenhouse. (NOTE: you can also set this inline with shortcode attributes.)
+*	Add shortcode or use the wizard to add a job board to any page or post.
 
 Know the code!
 
@@ -85,6 +85,12 @@ e.g.
 
 == Changelog ==
 
+= 1.5 =
+* Adding add job board media button and wizard to help construct your shortcode.
+* Bug fixes - compatability with ACF Pro.
+* Bug fixes - unexpected output.
+* Updates - requiring a url_token with user feedback on error.
+
 = 1.4 =
 * Adding support for location filter (include and exclude).
 * Adding support for office filter by name/title or id(include and exclude).
@@ -108,10 +114,11 @@ e.g.
 
 == Screenshots ==
 
-1. Shortcode in action.
+1. Shortcode in action. Notice the 'Add Greenhouse Job Board' button.
 2. View the job board list.
 3. Click to view description of each job.
 4. Click to apply for each job.
+5. View the 'Add Greenhosuse Job Board' button modal wizard. This will allow you to easily configure your board with all the settings. It will insert the configured shortcode into the content editor.
 
 
 == Frequently Asked Questions ==
--- admin/js/greenhouse-job-board-admin.js
+++ admin/js/greenhouse-job-board-admin.js
@@ -28,5 +28,81 @@
 	 * for any particular page. Though other scripts in WordPress core, other plugins, and other themes may
 	 * be doing this, we should try to minimize doing that in our own work.
 	 */
+	$( window ).load(function() {
+				
+		$('.wp-admin').on('click', '.insert-greenhouse-shortcode-button', function(e){
+			e.preventDefault();
+			
+			var shortcode = '[greenhouse';
+			
+			//url token
+			var url_token = $('#url_token').val();
+			if (url_token) {
+				shortcode += ' url_token="' + url_token + '"';
+			}
+			//API key
+			var api_key = $('#api_key').val();
+			if (api_key) {
+				shortcode += ' api_key="' + api_key + '"';
+			}
+			
+			//text strings
+			var apply_now = $('#apply_now').val();
+			if (apply_now) {
+				shortcode += ' apply_now="' + apply_now + '"';
+			}
+			var apply_now_cancel = $('#apply_now_cancel').val();
+			if (apply_now_cancel) {
+				shortcode += ' apply_now_cancel="' + apply_now_cancel + '"';
+			}
+			var read_full_desc = $('#read_full_desc').val();
+			if (read_full_desc) {
+				shortcode += ' read_full_desc="' + read_full_desc + '"';
+			}
+			var hide_full_desc = $('#hide_full_desc').val();
+			if (hide_full_desc) {
+				shortcode += ' hide_full_desc="' + hide_full_desc + '"';
+			}
+			
+			//hide_forms
+			if ( !$('#hide_forms').is(':checked') ) {
+				shortcode += ' hide_forms="true"';
+			}
+			
+			//department filter
+			var department_filter = $('#department_filter').val();
+			if (department_filter) {
+				shortcode += ' department_filter="' + department_filter + '"';
+			}
+			
+			//job filter
+			var job_filter = $('#job_filter').val();
+			if (job_filter) {
+				shortcode += ' job_filter="' + job_filter + '"';
+			}
+			
+			//office filter
+			var office_filter = $('#office_filter').val();
+			if (office_filter) {
+				shortcode += ' office_filter="' + office_filter + '"';
+			}
+			
+			//location filter
+			var location_filter = $('#location_filter').val();
+			if (location_filter) {
+				shortcode += ' location_filter="' + location_filter + '"';
+			}
+			
+			shortcode += ']';
+						
+			window.send_to_editor(shortcode);
+			
+		});
+		
+		$('.include').on('click', function(e){
+			$('.section_' + $(this).data('include') ).toggle();
+		});
+		
+    });
 
 })( jQuery );
